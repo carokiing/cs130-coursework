32fd78d2d309010df31ac01a61e65f4ffbe37560
carokiing <carolynking@u.northwestern.edu>
2022-04-27 17:40:55 -0500

    tutorial 5 completed

---
+++ lectures/lecture10/01-style-property-demo/main.js
@@ -0,0 +1,27 @@
+
+const makeRed = () => {
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'Red';
+    document.querySelector('button').style.backgroundColor= 'red';
+};
+
+const makeBlue = () => {
+    console.log('Change background to blue');
+    document.querySelector('body').style.backgroundColor = 'blue';
+    document.querySelector('#btn2').style.backgroundColor='blue';
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('body').style.backgroundColor = 'pink';
+    document.querySelector('#btn3').style.backgroundColor='pink';
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('body').style.backgroundColor = 'orange';
+    document.querySelector('#btn4').style.backgroundColor='orange';
+};
+

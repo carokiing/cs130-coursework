f17586c4e62f72d1eb47d98f9cd1d71cf6635580
carokiing <carolynking@u.northwestern.edu>
2022-05-04 17:28:37 -0500

    woohoo tutorial

--- homework/hw02/scripts.js
+++ homework/hw02/scripts.js
@@ -1,1 +1,18 @@
 console.log('Hello world!');
+
+const circle = '<circle cx ="416" cy="494" r="20" stroke="black" stroke-width="3" fill="red">'
+const square = '<rect x="291" y="70" width="40" height="40" stroke="black" stroke-width="3" fill="red">'
+const square = '<rect x="291" y="70" width="40" height="40" stroke="black" stroke-width="3" fill="red">'
+
+document.querySelector('svg').insertAdjacentHTML("beforeend, circle")
+
+document.querySelector('svg').insertAdjacentHTML("beforeend, square")
+
+document.querySelector('svg').insertAdjacentHTML("beforeend, triangle")
+
+const addShape= (ev) -> {
+    console.log(ev.clientX, ev.clientY);
+    console.log('Add shape')
+    const.circle='<circle cx ="416" cy="494" r="20" stroke="black" stroke-width="3" fill="red">'
+    document.querySelector('svg').insertAdjacentHTML("beforeend", circle)
+}
---
+++ lectures/lecture12/00-warmup/main.js
@@ -0,0 +1,19 @@
+const circle = '<circle cx="416" cy="494" r="20" stroke="black" stroke-width="3" fill="red"></circle>';
+const square = '<rect x="291" y="70" width="40" height="40" stroke="black" stroke-width="3" fill="pink"></rect>'
+const triangle = ' <polygon points="100,100 150,100 125,135" stroke="black" stroke-width="3" fill="teal"></polygon>';
+document.querySelector('svg').insertAdjacentHTML("beforeend", circle);
+document.querySelector('svg').insertAdjacentHTML("beforeend", square);
+document.querySelector('svg').insertAdjacentHTML("beforeend", triangle);
+
+
+const addShape= (ev) -> {
+    console.log(ev.clientX, ev.clientY);
+    console.log('Add shape')
+    const.circle='<circle cx ="416" cy="494" r="20" stroke="black" stroke-width="3" fill="red">'
+    document.querySelector('svg').insertAdjacentHTML("beforeend", circle)
+}
+/**
+ * Your job: when the user clicks the svg element, 
+ * draw the shape the corresponds with the controls 
+ * in the left-hand panel.
+ */
